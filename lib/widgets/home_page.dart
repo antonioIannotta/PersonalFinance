45ccb9f0d930c75b21231f7a8cfca91c7d6f0d88
antonioIannotta <antonio.iannotta.ai26@gmail.com>
2024-07-27 18:16:14 +0200

    feat: home page setup almost completed

--- lib/widgets/home_page.dart
+++ lib/widgets/home_page.dart
@@ -8,8 +8,79 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Home Page"), centerTitle: true),
-      body: const Center(
+      body: Center(
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: [
+            // Net Income Card
+            _buildCard("Net Income"), // Replace with actual data
+
+            // Total Income Card with Date Picker
+            _buildCardWithDatePicker(context),
+
+            // Expenses Card
+            _buildCard("Expenses"), // Replace with actual data
+          ],
+        ),
       ),
     );
+  }
+
+ Widget _buildCard(String title,[String value = ""]) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(fontSize: 18),
+            ),
+            const SizedBox(height: 8),
+            const SizedBox(height: 8),
+            Text(
+              value,
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCardWithDatePicker(BuildContext context) {
+    return Card(
+      elevation: 4,
+      child: Padding(
+        padding: const EdgeInsets.all(60.0),
+        child: Column(
+          children: [
+            const Text(
+              "Total Income",
+              style: TextStyle(fontSize: 18),
+            ),
+            ElevatedButton(
+              onPressed: () => _selectDate(context),
+              child: Text("Select Date"),
+            ),
+            // Display income for the selected date here
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime.now(),
+      firstDate: DateTime(2015, 8),
+      lastDate: DateTime(2101),
+    );
+    if (picked != null) {
+      // Handle the selected date (e.g., fetch income data for that date)
+    }
   }
 }
